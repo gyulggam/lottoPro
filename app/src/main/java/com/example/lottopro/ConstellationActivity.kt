e29717776c02aee9ec67929c6ac928641d385e63
HS <48476325+rkd2849@users.noreply.github.com>
2020-09-18 21:31:27 +0900

    20200918 hs
    커커커ㅓ 밋~

---
+++ app/src/main/java/com/example/lottopro/ConstellationActivity.kt
@@ -0,0 +1,19 @@
+package com.example.lottopro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class ConstellationActivity : AppCompatActivity() {
+    var clickCount = 0;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constellation)
+
+        Toast.makeText(applicationContext, "광고수입 호로록", Toast.LENGTH_LONG).show();
+
+
+    }
+}
--- app/src/main/java/com/example/lottopro/MainActivity.kt
+++ app/src/main/java/com/example/lottopro/MainActivity.kt
@@ -7,20 +7,39 @@ import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Timber.plant(Timber.DebugTree())
         setContentView(R.layout.activity_main)
 
-        val sPageMoveBtn = findViewById<Button>(R.id.moveBtn) as Button
-        var sNextIntent = Intent()
+        var sIntent = Intent();
 
-        sPageMoveBtn.setOnClickListener{
-            sNextIntent = Intent(this@MainActivity, SelectLottoActivity::class.java)
-            startActivity(sNextIntent)
+        menuBtn1.setOnClickListener{
+            sIntent = Intent(this@MainActivity, ConstellationActivity::class.java  )
+            startActivity(sIntent);
+
+        }
+
+        menuBtn2.setOnClickListener{
+//            val sIntent = Intent(this@MainActivity, ConstellationActivity::class.java  )
+//            startActivity(intent);
+
+        }
+        menuBtn3.setOnClickListener{
+//            val sIntent = Intent(this@MainActivity, ConstellationActivity::class.java  )
+//            startActivity(intent);
+
+        }
+        menuBtn4.setOnClickListener{
+            val intent = Intent(this@MainActivity, SelectLottoActivity::class.java  )
+            startActivity(intent);
+
         }
     }
+
 }
