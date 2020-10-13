2737ef988a3ead8c5b659da8e57edace25f1d27e
HS <48476325+rkd2849@users.noreply.github.com>
2020-10-13 23:13:24 +0900

    20201013(HS) 메인수정
    메인수정

--- app/src/main/java/com/example/lottopro/MainActivity.kt
+++ app/src/main/java/com/example/lottopro/MainActivity.kt
@@ -2,34 +2,21 @@ package com.example.lottopro
 
 import android.content.Context
 import android.content.Intent
-import android.graphics.Color
 import android.os.Bundle
 import android.os.StrictMode
-import android.text.Spannable
-import android.text.SpannableString
-import android.text.style.ForegroundColorSpan
 import android.util.TypedValue
-import android.view.Gravity
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.GridView
-import android.widget.LinearLayout
-import android.widget.Toast
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
-import androidx.core.view.isEmpty
 import com.example.lottopro.Adapter.ButtonAdapter
 import com.example.lottopro.DataBase.SqlHelper
 import com.example.lottopro.Str.LottoNum
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.MobileAds
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.activity_select_lotto.*
-import kotlinx.android.synthetic.main.constellation.*
 import kotlinx.android.synthetic.main.main.*
-import kotlinx.android.synthetic.main.random_lotto.*
-import kotlinx.android.synthetic.main.main.adView
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -49,24 +36,29 @@ class MainActivity : AppCompatActivity() {
 
 
         // Disable the `NetworkOnMainThreadException` and make sure it is just logged.
-        StrictMode.setThreadPolicy(StrictMode.ThreadPolicy.Builder().detectAll().penaltyLog().build())
+        StrictMode.setThreadPolicy(StrictMode.ThreadPolicy.Builder().detectAll().penaltyLog()
+            .build())
 
 
         // 로또 번호
         var slottoApi = LottoNumberMaker.getNumber()?.toTypedArray()
+
+
+
         // 로또 회차
         var slottoApiCount = LottoNumberMaker.getCount()
         // 로또 날짜
-        var slottoApiData = LottoNumberMaker.getDate().replace("-",".")
+        var slottoApiData = LottoNumberMaker.getDate().replace("-", ".")
 
 
-//        Toast.makeText(applicationContext, "날짜 ${slottoApiData}", Toast.LENGTH_LONG).show()
+//        Toast.makeText(applicationContext, "날짜 ${slottoApi}", Toast.LENGTH_LONG).show()
 
-        textView1.setText("$slottoApiData")
-        textView2.setText("${slottoApiCount}회차")
+
+        textView1.text = "$slottoApiData"
+        textView2.text = "${slottoApiCount}회차"
 
         val sAddGridPram = ViewGroup.LayoutParams(
-            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT,
             ViewGroup.LayoutParams.WRAP_CONTENT,
         )
 
@@ -85,22 +77,22 @@ class MainActivity : AppCompatActivity() {
         sAddGrid.adapter = sAdapter
 
 
-        var sIntent = Intent();
+        var sIntent = Intent()
         button1.setOnClickListener{
             sIntent = Intent(this@MainActivity, ConstellationActivity::class.java)
-            startActivity(sIntent);
+            startActivity(sIntent)
         }
         button2.setOnClickListener{
             sIntent = Intent(this@MainActivity, RandomLottoActivity::class.java)
-            startActivity(sIntent);
+            startActivity(sIntent)
         }
         button3.setOnClickListener{
             sIntent = Intent(this@MainActivity, PatternLottoActivity::class.java)
-            startActivity(sIntent);
+            startActivity(sIntent)
         }
         button4.setOnClickListener{
             sIntent = Intent(this@MainActivity, SelectLottoActivity::class.java)
-            startActivity(sIntent);
+            startActivity(sIntent)
         }
 
     }
@@ -127,27 +119,34 @@ class MainActivity : AppCompatActivity() {
         )
 
         for ((index, value) in gLottoList.withIndex()) {
-            lateinit var sLottoList : Array<String>
-            val sAddGrid: GridView = GridView(this)
 
-            var sStr = value.number
 
-            sAddGrid.layoutParams = sAddGridPram
-            sAddGrid.numColumns = 6
-            sAddGrid.background = ContextCompat.getDrawable(this, R.drawable.lotto_grid_view)
-            sAddGrid.horizontalSpacing = 10
-            sAddGrid.verticalSpacing = 10
-            saveLottoNum.addView(sAddGrid)
+            if(index == gLottoList.size-1){
+                lateinit var sLottoList : Array<String>
+                val sAddGrid: GridView = GridView(this)
 
-            if (sStr !== null) {
-                sLottoList= sStr?.split(",").toTypedArray()
+                var sStr = value.number
+
+                sAddGrid.layoutParams = sAddGridPram
+                sAddGrid.numColumns = 6
+                sAddGrid.background = ContextCompat.getDrawable(this, R.drawable.lotto_grid_view)
+
+
+                sAddGrid.horizontalSpacing = 20
+                saveLottoNum.addView(sAddGrid)
+                if (sStr !== null) {
+                    sLottoList= sStr.split(",").toTypedArray()
+                }
+
+                if (sLottoList != null) {
+                    var sAdapter = ButtonAdapter(this, sLottoList)
+
+                    sAddGrid.adapter = sAdapter
+                }
             }
 
-            if (sLottoList != null) {
-                var sAdapter = ButtonAdapter(this, sLottoList)
 
-                sAddGrid.adapter = sAdapter
-            }
+
         }
     }
 
@@ -158,7 +157,7 @@ class MainActivity : AppCompatActivity() {
         left: Float? = null,
         top: Float? = null,
         right: Float? = null,
-        bottom: Float? = null
+        bottom: Float? = null,
     ) {
         layoutParams<ViewGroup.MarginLayoutParams> {
             left?.run { leftMargin = dpToPx(this) }
